78c38bbd5beacbac09b1dfe8b492eb8852e223e1
tharindu ishan <ishanthari96@gmail.com>
2024-01-28 14:27:42 +0530

    Make sign in page

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
-
+from forms import RegistrationForm,LoginForm
 app=Flask(__name__)
+
+app.config['SECRET_KEY']='d74680c7dd28ba85b126fd4c10f684a8'
 
 posts=[
     {
@@ -24,7 +26,17 @@ def home():
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html',title='About')
+
+@app.route('/register')
+def register():
+    form=RegistrationForm()
+    return render_template('register.html',title='Register',form=form)
+
+@app.route('/login')
+def login():
+    form=LoginForm()
+    return render_template('login.html',title='Login',form=form)
 
 if __name__=='__main__':
     app.run(debug=True)
