09d08587d0c57d8ae1d6f9f5f865942d9319768b
tharindu ishan <ishanthari96@gmail.com>
2024-01-30 09:43:32 +0530

    Make user authentication part

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField ,SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length,Email,EqualTo
+from wtforms.validators import DataRequired, Length,Email,EqualTo,ValidationError
+from flaskblog.models import User
 
 class RegistrationForm(FlaskForm):
     username=StringField('Username',validators=[DataRequired(),
@@ -13,6 +14,15 @@ class RegistrationForm(FlaskForm):
     confirm_password=PasswordField('Cofirm Password',validators=[DataRequired(),EqualTo('password')])
 
     submit=SubmitField('Sign Up')
+
+    def validate_username(self,username):
+        user=User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That user name is already taken')
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is already taken')
 
 class LoginForm(FlaskForm):
     
